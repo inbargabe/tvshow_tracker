ec447aa10cf20c2645c2b565e126590ffba4c9a4
Inbar Gabe <inbarg@aidoc.com>
2025-08-15 16:43:52 +0300

    fixed typo in decorator

--- infra/api.py
+++ infra/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, current_app
 import boto3
 from botocore.exceptions import ClientError
-from utils.decorators import log_api_call, track_metrics
+from utils.decorator import log_api_call, track_metrics
 import logging
 
 logger = logging.getLogger(__name__)
